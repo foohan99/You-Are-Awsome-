a5e17ace2732f85990cba4151e3d6863bbe2afe5
Ken <ken@feehan4.com>
2021-12-17 21:51:19 -0800

    Initial Commit

---
+++ You Are Awsome!UITests/You_Are_Awsome_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  You_Are_Awsome_UITestsLaunchTests.swift
+//  You Are Awsome!UITests
+//
+//  Created by Ken Feehan on 12/17/21.
+//
+
+import XCTest
+
+class You_Are_Awsome_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
